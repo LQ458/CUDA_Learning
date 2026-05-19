08d6cda3d244dc568153e2a4b5432d775cea55b2
LQ458 <lqn458@gmail.com>
2026-05-20 00:13:17 +0800

    Update template.cu

--- ece408-labs/MP1/template.cu
+++ ece408-labs/MP1/template.cu
@@ -5,8 +5,8 @@
 #include <assert.h>
 
 __global__ void vecAdd(float *in1, float *in2, float *out, int len) {
-  //@@ Insert code to implement vector addition here
-  for(int i = 0; i < len; i++){
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < len) {
     out[i] = in1[i] + in2[i];
   }
 }
