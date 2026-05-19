cc0374a9aa9bf9de0c5e7526bc9b1e3909eb2bb8
LQ458 <lqn458@gmail.com>
2026-05-19 14:08:00 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git config --global user.email "lqn458@gmail.com"
+git config --global user.name "LQ458"
+
+wget -qO- https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub | apt-key add -
+echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64 /" > /etc/apt/sources.list.d/cuda.list
+apt update && apt install -y nsight-compute 2>/dev/null
+
+cd ~
+[ -d projects ] || git clone https://github.com/lq458/CUDA_Learning.git projects
+cd ~/projects
+git remote set-url origin "$(cat /mnt/.git_token)@github.com/lq458/CUDA_Learning.git"
+
+echo "Ready. nvcc: $(nvcc --version | head -1)"
+ncu --version | head -1
